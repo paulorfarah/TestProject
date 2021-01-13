99037b5b0c55f4c7487434f1d13c0893c6e0471c
Paulo Farah <paulorfarah@gmail.com>
2021-01-13 17:43:02 -0300

    first commit

---
+++ src/testproject/Main.java
@@ -0,0 +1,9 @@
+package testproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.printArray();
+	}
+}
---
+++ src/testproject/Test.java
@@ -0,0 +1,28 @@
+package testproject;
+
+import java.util.Random;
+
+public class Test {
+	private static final int MAX=100;
+	int[][][] a=new int[MAX][MAX][MAX];
+	public Test() {
+		Random r=new Random();
+		for (int i = 0; i < MAX; i++) {
+			for (int j = 0; j < MAX; j++) {
+				for (int k = 0; k < MAX; k++) {
+					a[i][j][k]=r.nextInt(20);
+				}
+		    }
+		}
+	}
+	
+	public void printArray() {
+		for (int i = 0; i < MAX; i++) {
+			for (int j = 0; j < MAX; j++) {
+				for (int k = 0; k < MAX; k++) {
+					System.out.println(a[i][j][k]);
+				}
+			}
+		}
+	}
+}
